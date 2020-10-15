e7e7bcc6f377dea928769743ff2e056093bc1e53
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-10-15 12:51:31 +0530

    Update gallery.tsx

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,65 +1,28 @@
-import React from "react"
-
-//import { useStaticQuery, graphql } from "gatsby"
-
 import "mdbreact"
-
+import { MDBLightbox } from "mdbreact"
+import React, { useState } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import "../components/pages/gallery/gallery.sass"
-
 const IndexPage = () => {
+  const [heading, setHeading] = useState<any>([
+    {
+      src:
+        "https://firebasestorage.googleapis.com/v0/b/history-class.appspot.com/o/flamelink%2Fmedia%2F7Q6epfACjVuPBKklcmgJ_photo1.webp?alt=media&token=5d6964ce-051c-4b1d-a1a9-329808d07242",
+    },
+    {
+      src:
+        "https://firebasestorage.googleapis.com/v0/b/history-class.appspot.com/o/flamelink%2Fmedia%2FLD4Ek4CHywSns0lVQ9tK_photo2.webp?alt=media&token=c92fc21e-fff2-4792-91bb-6bd73aa1d6a2",
+    },
+    {
+      src:
+        "https://firebasestorage.googleapis.com/v0/b/history-class.appspot.com/o/flamelink%2Fmedia%2FsooxC3njuiQlSR0ooaPo_photo3.webp?alt=media&token=fecffdef-2f6a-488b-a921-29a11419a41b",
+    },
+  ])
   return (
     <Layout>
       <SEO title="Gallery" />
-      <div className="imgContainer">
-        <img
-          src="https://mdbootstrap.com/img/Others/documentation/img(118)-mini.jpg"
-          className="img-fluid z-depth-1"
-          alt="women"
-        />
-        <img
-          src="https://mdbootstrap.com/img/Others/documentation/img(118)-mini.jpg"
-          className="img-fluid z-depth-1"
-          alt="women"
-        />
-        <img
-          src="https://mdbootstrap.com/img/Others/documentation/img(118)-mini.jpg"
-          className="img-fluid z-depth-1"
-          alt="women"
-        />
-        <img
-          src="https://mdbootstrap.com/img/Others/documentation/img(118)-mini.jpg"
-          className="img-fluid z-depth-1"
-          alt="women"
-        />
-        <img
-          src="https://mdbootstrap.com/img/Others/documentation/img(118)-mini.jpg"
-          className="img-fluid z-depth-1"
-          alt="women"
-        />
-        <img
-          src="https://mdbootstrap.com/img/Others/documentation/img(118)-mini.jpg"
-          className="img-fluid z-depth-1"
-          alt="women"
-        />
-        <img
-          src="https://mdbootstrap.com/img/Others/documentation/img(118)-mini.jpg"
-          className="img-fluid z-depth-1"
-          alt="women"
-        />
-        <img
-          src="https://mdbootstrap.com/img/Others/documentation/img(118)-mini.jpg"
-          className="img-fluid z-depth-1"
-          alt="women"
-        />
-        <img
-          src="https://mdbootstrap.com/img/Others/documentation/img(118)-mini.jpg"
-          className="img-fluid z-depth-1"
-          alt="women"
-        />
-      </div>
+      <MDBLightbox md="4" images={heading} noMargins />
     </Layout>
   )
 }
