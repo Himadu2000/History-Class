995e8346a29253c787ec8fcac5ffde20291ac8c8
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-10-16 14:12:11 +0530

    Update gallery.tsx

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,59 +1,38 @@
 import { graphql } from "gatsby"
-import { MDBLightbox } from "mdbreact"
-import React, { useEffect, useState } from "react"
+import { MDBLightbox, MDBSpinner } from "mdbreact"
+import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 interface props {
   data: {
-    flamelinkGalleryContent: {
-      src: string
-      alt: string
-    }[]
+    allFlamelinkGalleryContent: {
+      nodes: []
+    }
   }
 }
 
-const IndexPage = ({ data }: props) => {
-  const [heading, setHeading] = useState<any>([
-    {
-      src:
-        "https://firebasestorage.googleapis.com/v0/b/history-class.appspot.com/o/flamelink%2Fmedia%2F7Q6epfACjVuPBKklcmgJ_photo1.webp?alt=media&token=5d6964ce-051c-4b1d-a1a9-329808d07242",
-    },
-    {
-      src:
-        "https://firebasestorage.googleapis.com/v0/b/history-class.appspot.com/o/flamelink%2Fmedia%2FLD4Ek4CHywSns0lVQ9tK_photo2.webp?alt=media&token=c92fc21e-fff2-4792-91bb-6bd73aa1d6a2",
-    },
-    {
-      src:
-        "https://firebasestorage.googleapis.com/v0/b/history-class.appspot.com/o/flamelink%2Fmedia%2FsooxC3njuiQlSR0ooaPo_photo3.webp?alt=media&token=fecffdef-2f6a-488b-a921-29a11419a41b",
-    },
-  ])
-
-  useEffect(() => {
-    const items = []
-    console.log(data.flamelinkGalleryContent)
-    // data.flamelinkGalleryContent.map(item => {
-    //   items.push({ src: item.url })
-    // })
-    // setHeading()
-    console.log(items)
-  }, [])
-
-  return (
-    <Layout>
-      <SEO title="Gallery" />
-      {typeof window !== "undefined" ? (
-        <MDBLightbox md="4" images={heading} noMargins />
-      ) : null}
-    </Layout>
-  )
-}
+const IndexPage = ({ data }: props) => (
+  <Layout>
+    <SEO title="Gallery" />
+    {typeof window !== "undefined" && data.allFlamelinkGalleryContent.nodes ? (
+      <MDBLightbox
+        md="4"
+        images={data.allFlamelinkGalleryContent.nodes}
+        noMargins
+      />
+    ) : (
+      <MDBSpinner multicolor />
+    )}
+  </Layout>
+)
 
 export const query = graphql`
   {
-    flamelinkGalleryContent {
-      src
-      # alt
+    allFlamelinkGalleryContent {
+      nodes {
+        src
+      }
     }
   }
 `
